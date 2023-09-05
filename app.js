4a80fa060c73546886d35d3b94d16eb100e6c377
codeschlau <laurent@schiltz.info>
2023-09-05 20:35:55 +0200

    zweiter

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const createButton = document.getElementById('createButton');
-    const roomCodeInput = document.getElementById('roomCodeInput');
-    const roomCodeDisplay = document.getElementById('roomCode');
+    const wordInput = document.getElementById('wordInput');
+    const addWordButton = document.getElementById('addWordButton');
+    const wordCloud = document.getElementById('wordCloud');
 
-    createButton.addEventListener('click', () => {
-        // Hiermit kannst du einen neuen Raum erstellen und den Raumcode anzeigen
-        fetch('create_room.php')
-            .then(response => response.text())
-            .then(roomCode => {
-                roomCodeDisplay.textContent = roomCode;
-            });
-    });
-    joinButton.addEventListener('click', () => {
-        const roomCode = roomCodeInput.value;
-        // Simuliere das Hinzufügen von Wörtern zur Wortwolke
-        const newWord = prompt('Enter a word:');
-        if (newWord) {
-            // Hier könntest du AJAX verwenden, um das Wort an den Server zu senden
-            // und die aktualisierte Wortwolke abzurufen.
-            wordCloud.textContent += ' ' + newWord;
+    addWordButton.addEventListener('click', () => {
+        const word = wordInput.value;
+        if (word) {
+            // Füge das Wort zur Wortwolke hinzu
+            wordCloud.textContent += ' ' + word;
+            wordInput.value = ''; // Leere das Eingabefeld
         }
     });
 });
-
--- server.php
+++ server.php
@@ -1,7 +1,14 @@
 <?php
-// Simuliere das Speichern der Wörter und Generieren der Wortwolke
-$words = ['apple', 'banana', 'cherry', 'dog', 'elephant']; // Beispielwörter
-shuffle($words); // Zufällige Reihenfolge
-$wordCloud = implode(' ', $words);
-echo json_encode(['wordCloud' => $wordCloud]);
+$wordCloud = isset($_POST['wordCloud']) ? $_POST['wordCloud'] : '';
+
+if ($wordCloud) {
+    // Speichere die Wortwolke in einer Datei oder einer Datenbank
+    // Hier verwenden wir eine Datei, um es einfach zu halten
+    file_put_contents('word_cloud.txt', $wordCloud);
+    echo "Word cloud saved successfully.";
+} else {
+    // Lade die gespeicherte Wortwolke
+    $wordCloud = file_get_contents('word_cloud.txt');
+    echo $wordCloud;
+}
 ?>
